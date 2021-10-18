c8bf62564429b747baa107013babcccfdc44a47d
Z572 <873216071@qq.com>
2021-10-19 00:41:55 +0800

    gnu: Add texmacs-with-guile3.
    * Z572/packages/text-editors.scm (texmacs-with-guile3): New variable.

---
+++ Z572/packages/text-editors.scm
@@ -0,0 +1,40 @@
+(define-module (Z572 packages text-editors)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix utils)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages text-editors))
+
+(define-public texmacs-with-guile3
+  (let ((commit "77d3fa5df0d4a7aa1def6a956eb2972ef173ac33")
+        (revision "1"))
+    (package
+      (inherit texmacs)
+      (name "texmacs-with-guile3")
+      (version (git-version "2.1" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/mgubi/texmacs")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1f95w66da13hkk2svfxgmz5arqy968hvhx9jlwmv8gc04swdcxjk"))))
+      (inputs
+       `(("guile" ,guile-3.0-latest)
+         ,@(package-inputs texmacs)))
+      (arguments (substitute-keyword-arguments (package-arguments texmacs)
+                   ((#:phases phases)
+                    `(modify-phases ,phases
+                       (add-after 'unpack 'chdir-src
+                         (lambda _ (chdir "src")))
+                       (add-before 'reset-gzip-timestamps 'make-files-writable
+                         (lambda* (#:key outputs #:allow-other-keys)
+                           ;; Make sure .gz files are writable so that the
+                           ;; 'reset-gzip-timestamps' phase can do its work.
+                           (let ((out (assoc-ref outputs "out")))
+                             (for-each make-file-writable
+                                       (find-files out "\\.gz$")))))))
+                   ((#:configure-flags flags ''())
+                    `(cons "--enable-guile2" ,flags)))))))
